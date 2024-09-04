b784b7959dbcadb6427bfebd1cfe9ae76abb4398
cardinal-archive <editoe.archive@gmail.com>
2024-09-03 23:01:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,48 @@
-const notes = {
-    'C': 261.63,
-    'C#': 277.18,
-    'D': 293.66,
-    'D#': 311.13,
-    'E': 329.63,
-    'F': 349.23,
-    'F#': 369.99,
-    'G': 392.00,
-    'G#': 415.30,
-    'A': 440.00,
-    'A#': 466.16,
-    'B': 493.88,
-    'C2': 523.25
-};
+let currentOctave = 3;
 
-const keys = document.querySelectorAll('.key');
-let audioContext = new (window.AudioContext || window.webkitAudioContext)();
-let oscillator;
+document.getElementById('octave-down').addEventListener('click', () => {
+    if (currentOctave > 1) {
+        currentOctave--;
+        updateKeys();
+    }
+});
 
-keys.forEach(key => {
-    key.addEventListener('mousedown', () => {
-        playTone(key.dataset.note);
-        key.classList.add('pressed');
-    });
+document.getElementById('octave-up').addEventListener('click', () => {
+    if (currentOctave < 7) {
+        currentOctave++;
+        updateKeys();
+    }
+});
 
-    key.addEventListener('mouseup', () => {
-        stopTone();
-        key.classList.remove('pressed');
-    });
-
-    key.addEventListener('mouseleave', () => {
-        stopTone();
-        key.classList.remove('pressed');
+document.querySelectorAll('.white-key, .black-key').forEach(key => {
+    key.addEventListener('click', () => {
+        playNote(key.dataset.note);
     });
 });
 
-function playTone(note) {
-    const frequency = notes[note];
-    if (oscillator) {
-        oscillator.stop();
-    }
-    oscillator = audioContext.createOscillator();
-    oscillator.type = 'sine';
-    oscillator.frequency.setValueAtTime(frequency, audioContext.currentTime);
-    oscillator.connect(audioContext.destination);
-    oscillator.start();
+function updateKeys() {
+    document.querySelectorAll('.white-key, .black-key').forEach(key => {
+        let note = key.dataset.note;
+        key.dataset.note = note.slice(0, -1) + currentOctave;
+    });
 }
 
-function stopTone() {
-    if (oscillator) {
-        oscillator.stop();
-        oscillator.disconnect();
-    }
+function playNote(note) {
+    let audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    let oscillator = audioContext.createOscillator();
+    let gainNode = audioContext.createGain();
+    oscillator.connect(gainNode);
+    gainNode.connect(audioContext.destination);
+    oscillator.frequency.setValueAtTime(getFrequency(note), audioContext.currentTime);
+    oscillator.start();
+    oscillator.stop(audioContext.currentTime + 1);
+}
+
+function getFrequency(note) {
+    const A4 = 440;
+    const semitoneRatio = Math.pow(2, 1/12);
+    const notes = {'C': -9, 'C#': -8, 'D': -7, 'D#': -6, 'E': -5, 'F': -4, 'F#': -3, 'G': -2, 'G#': -1, 'A': 0, 'A#': 1, 'B': 2};
+    const [letter, octave] = [note.slice(0, -1), note.slice(-1)];
+    const noteNumber = notes[letter] + (octave - 4) * 12;
+    return A4 * Math.pow(semitoneRatio, noteNumber);
 }
